2019af1a790005dbaca4f6ec7ba375be07a7b02b
Ayush Singh <60828254+Ayush036@users.noreply.github.com>
2021-12-22 14:43:10 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="src",
+    version="0.0.1",
+    description="For MLOPS",
+    author="Ayush",
+    packages=find_packages(),
+    license="MIT"
+)
