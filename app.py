d5b1b2080b352325d8936b8c1ce818ea6e0cb387
Ayush Singh <60828254+Ayush036@users.noreply.github.com>
2021-12-23 21:38:01 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import yaml
 import joblib
 import numpy as np
 import requests
-from flask import render_template, request
+from flask import render_template, request, jsonify
 
-params_parh="params.yaml"
+params_path="params.yaml"
 webapp_root="webapp"
 
 static_dir=os.path.join(webapp_root, "static")
@@ -15,10 +15,48 @@ template_dir=os.path.join(webapp_root,"templates")
 
 app=Flask(__name__,static_folder=static_dir,template_folder=template_dir)
 
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config=yaml.safe_load(yaml_file)
+    return config
+
+def predict(data):
+    config=read_params(params_path)
+    model_dir_path= config["webapp_model_dir"]
+    model=joblib.load(model_dir_path)
+    prediction=model.predict(data)
+    print(prediction)
+    return prediction[0]
+def api_response(request):
+    try:
+        data=np.array([list(request.json.values())])
+        response=predict(data)
+        response={"response":response}
+    except Exception as e:
+        print(e)
+        error={"error":"something went wrong"}
+        return error
+
+
+
+
 @app.route("/", methods=["GET","POST"])
 def index():
     if request.method=="POST":
-        pass
+        try:
+            if request.form:
+                data=dict(request.form).values()
+                data=[list(map(float,data))]
+                response=predict(data)
+                return render_template("index.html", response=response)
+            elif request.json:
+                response=api_response(request)
+                return  jsonify(response)
+        except Exception as e:
+            print(e)
+            error= {"error": "something went wrong"}
+            return render_template("404.html", error=error)
+
     else:
         return render_template("index.html")
 
