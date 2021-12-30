4446d3c83eb7b08f1a806046c7a7bb9cb06c7ca8
Ayush Singh <60828254+Ayush036@users.noreply.github.com>
2021-12-30 20:23:28 +0530

    app updated

--- app.py
+++ app.py
@@ -1,65 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 import os
-import yaml
-import joblib
 import numpy as np
-import requests
-from flask import render_template, request, jsonify
-
-params_path="params.yaml"
-webapp_root="webapp"
-
-static_dir=os.path.join(webapp_root, "static")
-template_dir=os.path.join(webapp_root,"templates")
+from prediction_service import prediction
 
 
-app=Flask(__name__,static_folder=static_dir,template_folder=template_dir)
+webapp_root = "webapp"
 
-def read_params(config_path):
-    with open(config_path) as yaml_file:
-        config=yaml.safe_load(yaml_file)
-    return config
+static_dir = os.path.join(webapp_root, "static")
+template_dir = os.path.join(webapp_root, "templates")
 
-def predict(data):
-    config=read_params(params_path)
-    model_dir_path= config["webapp_model_dir"]
-    model=joblib.load(model_dir_path)
-    prediction=model.predict(data)
-    print(prediction)
-    return prediction[0]
-def api_response(request):
-    try:
-        data=np.array([list(request.json.values())])
-        response=predict(data)
-        response={"response":response}
-    except Exception as e:
-        print(e)
-        error={"error":"something went wrong"}
-        return error
+app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
 
 
-
-
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method=="POST":
+
+    if request.method == "POST":
         try:
             if request.form:
-                data=dict(request.form).values()
-                data=[list(map(float,data))]
-                response=predict(data)
+                dict_req = dict(request.form)
+                response = prediction.form_response(dict_req)
                 return render_template("index.html", response=response)
             elif request.json:
-                response=api_response(request)
-                return  jsonify(response)
+                response = prediction.api_response(request.json)
+                return jsonify(response)
+
         except Exception as e:
             print(e)
-            error= {"error": "something went wrong"}
-            return render_template("404.html", error=error)
+            error = {"error": "Something went wrong!! Try again later!"}
+            error = {"error": e}
 
+            return render_template("404.html", error=error)
     else:
         return render_template("index.html")
 
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
